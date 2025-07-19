3c3feaa1f0a526781d057ea37a6987592925b377
deepaksingh023 <dsinghrana71@gmail.com>
2025-07-19 22:46:01 +0530

    forgot some code

--- index.js
+++ index.js
@@ -237,3 +237,10 @@ io.on('connection', (socket) => {
     }
   });
 });
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
